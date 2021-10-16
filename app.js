81c4a6dd0676ec9c9a9f72025dacc8fc3be3da57
gobbyboss <gobbyboss@gmail.com>
2021-10-16 17:45:28 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const token = require("./token.json");
+client.login(token.token);
+
+client.on('ready', () => console.log('The Bot is ready!'));
+
+client.on('message', message => {
+ 
+    if (message.content === 'valorant'){
+        message.react('👑');
+        message.channel.send('Yawn');
+    }
+})
