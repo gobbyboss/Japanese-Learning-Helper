e7312bc9c714bca5ed17a59aa69e6c01e36211da
gobbyboss <gobbyboss@gmail.com>
2021-10-17 19:39:44 -0500

    V1.11 Quiz Added

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,12 +1,16 @@
-const fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('n5vocaben').setDescription('Take an N5 vocab quiz, reply in english'),
+]
+	.map(command => command.toJSON());
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
